fc8a5e40af5462bf7b121aba1994244461aa0003
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2023-08-04 12:35:08 -0400

    up

--- learn-spatie/app/Http/Controllers/Controller.php
+++ learn-spatie/app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
@@ -17,5 +18,17 @@ class Controller extends BaseController
     public function showRegisterForm()
     {
         return view('auth.register');
+    }
+    public function store(Request $request)
+    {
+     $name = $request -> name;
+     $email = $request -> email;
+     $password = $request -> password;
+     
+     $user = User::create([
+        'name' => $name,
+        'email' => $email,
+        'password' => bcrypt($password)
+     ]);
     }
 }
